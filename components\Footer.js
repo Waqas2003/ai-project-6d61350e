b27c70554263bb497ac166a29e54c249e0a5f2c6
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-11 17:50:02 +0500

    Add components\Footer.js

---
+++ components\Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div className="bg-gray-200 h-screen p-4">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold">Swat Beauty</h2>
+        <p>Copyright 2023 Swat Beauty. All rights reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
